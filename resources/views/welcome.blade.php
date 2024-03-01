9618ad2013da7f69ca5c2714f015cad323b4197e
Nekkoy <37345613+Nekkoy@users.noreply.github.com>
2024-03-01 12:16:17 +0200

    Исправлено название
    Удалена ссылка tg
    Исправлено название

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <h1>MikBill support bot</h1>
+                <h1>Support bot</h1>
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
 
                 </div>
@@ -49,10 +49,6 @@
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                             </svg>
-
-                            <a href="https://t.me/kagatan" class="ml-1 underline">
-                                Shop
-                            </a>
                         </div>
                     </div>
 
